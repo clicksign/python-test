749fa6d22771a860d2db59e8613d339a6262b322
MartinsMessias <messiasads2017@gmail.com>
2023-06-03 16:40:15 -0300

    :white_check_mark: Add test for Orders & fix models

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import ProtectedError
 
 
 class Category(models.Model):
@@ -6,3 +7,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def delete(self, *args, **kwargs):
+        try:
+            products = self.product_set.all().values_list('id', flat=True)
+            if products:
+                raise ProtectedError("Category is associated with products and cannot be deleted.", products)
+            super().delete(*args, **kwargs)
+        except ProtectedError as e:
+            raise e
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.db.models import ProtectedError
 from apps.categories.models import Category
 
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+    def delete(self, *args, **kwargs):
+        try:
+            orders = self.order_items.all().values_list('order_id', flat=True)
+            if orders:
+                raise ProtectedError("Product is associated with orders and cannot be deleted.", orders)
+            super().delete(*args, **kwargs)
+        except ProtectedError as e:
+            raise e
