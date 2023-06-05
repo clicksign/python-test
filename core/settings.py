29c0cbd3e2940aa64b96778939c3f5a2e7f166ae
MartinsMessias <messiasads2017@gmail.com>
2023-06-05 20:35:14 -0300

    :memo: Add SwaggerUI for Api docs

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # Third-Party Apps
     'rest_framework',
+    'drf_spectacular',
 
     # Local Apps
     'apps.users.apps.UsersConfig',
@@ -132,3 +133,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'API Documentation',
+    'DESCRIPTION': 'API for placing orders and managing products',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "persistAuthorization": True,
+        "displayOperationId": True,
+    }
+}
