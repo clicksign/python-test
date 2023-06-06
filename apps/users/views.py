d68422967b6bc2700e7a31c9c6c69a43718dbf69
MartinsMessias <messiasads2017@gmail.com>
2023-06-05 21:07:44 -0300

    :sparkles: Add serializers, views & routes for Users module

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from apps.users.serializers import CustomUserSerializer
+from apps.users.models import CustomUser
 
-# Create your views here.
+
+@extend_schema(tags=['Users'])
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = (IsAuthenticated, IsAdminUser)
+
+
+@extend_schema(tags=['Users'])
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = (IsAuthenticated, IsAdminUser)
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
@@ -8,6 +8,9 @@ urlpatterns = [
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # Apps
+    path('api/users/', include('apps.users.urls')),
 
     # Django Admin
     path('admin/', admin.site.urls),
