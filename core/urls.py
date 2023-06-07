8c5a07b678e96b282cd2f152b9d22001063f4056
MartinsMessias <messiasads2017@gmail.com>
2023-06-07 20:14:15 -0300

    :fire: Remove unused files & settings

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import include, path
 from core.jwt import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
@@ -15,9 +14,6 @@ urlpatterns = [
     path('api/categories/', include('apps.categories.urls')),
     path('api/products/', include('apps.products.urls')),
     path('api/orders/', include('apps.orders.urls')),
-
-    # Django Admin
-    path('admin/', admin.site.urls),
 
     # JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
