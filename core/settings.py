3cb900ca574a2ec077092a55dfd6d447eca53b5f
MartinsMessias <messiasads2017@gmail.com>
2023-06-07 20:14:15 -0300

    :fire: Remove unused files & settings

--- core/settings.py
+++ core/settings.py
@@ -94,13 +94,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config("POSTGRES_NAME", default="postgres"),
+        'HOST': config("POSTGRES_HOST", default="localhost"),
+        'PORT': config("POSTGRES_PORT", default=5432, cast=int),
+        'USER': config("POSTGRES_USER", default="postgres"),
+        'PASSWORD': config("POSTGRES_PASSWORD", default="postgres"),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
